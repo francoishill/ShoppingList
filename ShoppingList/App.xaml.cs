d61f27c73616bfc72a733e5370bd7d5a07f5bfbd
FrancoisLaptopDell <FrancoisLaptopDell@3d0ec201-b222-5b4a-9713-b14d0b865495>
2012-11-24 22:55:59 +0000

    git-svn-id: svn://localhost/shoppinglist/trunk@2 3d0ec201-b222-5b4a-9713-b14d0b865495

---
+++ ShoppingList/App.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using SharedClasses;
+
+namespace ShoppingList
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public static MainWindow mainwindow;
+
+		public static void ShowError(string err)
+		{
+			System.Windows.Forms.Application.EnableVisualStyles();
+			if (mainwindow == null)
+				System.Windows.Forms.MessageBox.Show(err, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+			else
+				mainwindow.Dispatcher.Invoke((Action)delegate
+				{
+					System.Windows.Forms.MessageBox.Show(mainwindow, err, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+				});
+		}
+
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			SingleInstanceApplicationManager<MainWindow>.CheckIfAlreadyRunningElseCreateNew(
+				(evt, mainwin) =>
+				{
+					mainwin.Dispatcher.Invoke((Action)delegate
+					{
+						var args = evt.CommandLineArgs.ToList();
+						args.RemoveAt(0);//Remove the EXE path
+						if (args.Count >= 1 && args[0].ToLower() == ShoppingList.MainWindow.cUriProtocolHandlerCommandlineArgument.ToLower())
+						{
+							string commandFromUri =
+								args[1].Substring(ShoppingList.MainWindow.cUriStartString.Length + 1).ToLower();//The +1 is because we must add the ':' character
+							if (commandFromUri == "show")
+							{
+								mainwin.Show();
+								mainwin.Topmost = !mainwin.Topmost;
+								mainwin.Topmost = !mainwin.Topmost;
+								mainwin.Activate();
+							}
+						}
+					});
+				},
+				(args, mainwin) =>
+				{
+					AppDomain.CurrentDomain.UnhandledException += (snder, exc) =>
+					{
+						Exception exception = (Exception)exc.ExceptionObject;
+						ShowError("Exception" + (exc.IsTerminating ? ", application will now exit" : "") + ":"
+							+ exception.Message + Environment.NewLine + exception.StackTrace);
+					};
+
+					ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
+					{
+						AutoUpdating.CheckForUpdates(null, null);
+					},
+					false);
+
+					//ApplicationRecoveryAndRestart.RegisterForRecoveryAndRestart(
+
+					mainwindow = mainwin;
+					mainwin.Show();
+				});
+		}
+	}
+}
---
+++ ShoppingList/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SharedClasses;
+using System.Windows.Interop;
+
+namespace ShoppingList
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, System.Windows.Forms.IWin32Window
+	{
+		public const string cUriStartString = "shoppinglist";
+		public const string cUriProtocolHandlerCommandlineArgument = "uriprotocolhandler";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			//string appPath = RegistryInterop.GetAppPathFromRegistry("ShoppingList");
+			//if (appPath != null && appPath == Environment.GetCommandLineArgs()[0])//This application is installed
+				RegistryInterop.AssociateUrlProtocolHandler(
+					cUriStartString,
+					"Shopping List with Firepuma",
+					"\"" + Environment.GetCommandLineArgs()[0] + "\" " + cUriProtocolHandlerCommandlineArgument + " \"%1\"");
+		}
+
+		public IntPtr Handle
+		{
+			get { return new WindowInteropHelper(this).Handle; }
+		}
+	}
+}
